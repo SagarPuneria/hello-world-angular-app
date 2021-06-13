8ec8022807296ff0034847808ddeb45fb7a8a93c
Sagar Puneria <Sagar.Puneria@gmail.com>
2021-06-13 19:32:00 +0530

    feat: Class Binding concept

--- src/app/courses3.component.ts
+++ src/app/courses3.component.ts
@@ -1,14 +1,20 @@
-// Adding Bootstrap concept
+// Adding Bootstrap and Class Binding concept
 import { Component } from "@angular/core";
 
 @Component({
     selector: 'courses',
     template: `
-        <button class="btn btn-primary">Save</button>
+        <!-- Adding Bootstrap concept -->
+        <!-- <button class="btn btn-primary">Save</button> -->
+        
+        <!-- Class Binding concept -->
+        <button class="btn btn-primary" [class.active]="isActive">Save</button>
     `
 })
 
 export class CoursesComponent {
+    // isActive = false; // DOCTYPE html: <button class="btn btn-primary">Save</button>
+    isActive = true; // DOCTYPE html: <button class="btn btn-primary active">Save</button>
 }
 
 /*

68ae14a4d1af5d2ac7af3ac01b60f11fa476f1b2
Sagar Puneria <Sagar.Puneria@gmail.com>
2021-06-24 13:28:54 +0530

    feat: Passing Event Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FavoriteComponentEventArgs } from './favorite/favorite.component';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +11,13 @@ export class AppComponent {
     title: 'hello-world-angular-app',
     isSelected: true
   }
-
+  // onFavoriteChange(isFavorite: any) { // isFavorite: any => Intellisense won't work in this method, if isFavorite is object.
+  // onFavoriteChange(eventArgs: FavoriteComponentEventArgs) { //OR onFavoriteChange(eventArgs: { newValue: boolean }) {
+  onFavoriteChange(eventArgs: FavoriteComponentEventArgs) { // To make FavoriteComponentEventArgs reusable across the app, import from FavoriteComponent.
+    console.log("Favorite changed:", eventArgs);
+  }
   /* imageUrl = "http://lorempixel.com/400/200/";
   onClick() {
     console.log("button was clicked");
-  }
-  onFavoriteChange() {
-    console.log("onFavoriteChange.............");
   } */
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'favorite',
@@ -9,6 +9,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class FavoriteComponent implements OnInit {
   // @Input() isFavorite: boolean; // First approach: Input Properties concept(Not preffered approach)
   @Input('isFavorite') isSelected: boolean; // Second approach: Aliasing Input Properties(Preffered approach)
+  @Output() change = new EventEmitter();
 
   constructor() { }
 
@@ -18,6 +19,15 @@ export class FavoriteComponent implements OnInit {
   onClick() {
     // this.isFavorite = !this.isFavorite; // (First approach: Not preffered approach)
     this.isSelected = !this.isSelected; // (Second approach: Preffered approach)
+
+    // Passing Event Data
+    // this.change.emit(this.isSelected); // Simple component, just passing boolean value
+    this.change.emit({ newValue: this.isSelected }); // Complex component, just passing object
   }
 
+}
+
+
+export interface FavoriteComponentEventArgs {
+  newValue: boolean
 }

0afc04ff12bf3e3c81b08bd063cf4fc0e902a956
Sagar Puneria <Sagar.Puneria@gmail.com>
2021-07-03 16:22:55 +0530

    feat: ngFor concept

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  courses = [1, 2];
+  course = [1, 2];
+  courses = [
+    { id: 1, name: 'course1' },
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+  ];
   viewMode = 'map';
 }

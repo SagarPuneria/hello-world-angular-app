ec3cf7145e3f3dbfdd04c53e266a0c4b8a31e3a7
Sagar Puneria <Sagar.Puneria@gmail.com>
2021-07-04 17:23:31 +0530

    feat: Creating Custom Directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputFormatDirective } from './input-format.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFormatDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/input-format.directive.ts
@@ -0,0 +1,31 @@
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appInputFormat]'
+})
+export class InputFormatDirective {
+
+  // @Input('format') format: string;
+
+  // if you have only one input properties(i.e.,format), 
+  // you can use selector(i.e.,appInputFormat) of that directive as an alias of that input property 
+  // and this simplifies the usages of your custom directive.
+  @Input('appInputFormat') format: string;
+
+  constructor(private el: ElementRef) { }
+
+  @HostListener('focus') onFocus() {
+    console.log("on Focus");
+  }
+
+  @HostListener('blur') onBlur() {
+    console.log("on Blur");
+    let value: string = this.el.nativeElement.value;
+
+    if (this.format == 'lowercase')
+      this.el.nativeElement.value = value.toLowerCase();
+    else
+      this.el.nativeElement.value = value.toUpperCase();
+  }
+
+}

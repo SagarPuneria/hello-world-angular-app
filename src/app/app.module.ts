355b420378c6f7a3999bd441e68767b2b183e5e5
Sagar Puneria <sagar.puneria@gmail.com>
2021-07-24 17:29:31 +0530

    feat: Creating Controls Programmatically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,9 @@ import { SignupFormComponent } from './signup-form/signup-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'signup-form',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup-form.component.css']
 })
 export class SignupFormComponent implements OnInit {
+
+  form = new FormGroup({
+    // If key has special character name like - then that key is not valid property in JS. 
+    // So then we need to put this key in single quotes.
+    // 'user-name':new FormControl()
+
+    username: new FormControl(),
+    password: new FormControl()
+  })
 
   constructor() { }
 

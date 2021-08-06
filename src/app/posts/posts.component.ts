e88e2b92d74a460cb15c15372331edcb6b847fef
Sagar Puneria <sagar.puneria@gmail.com>
2021-08-06 22:41:41 +0530

    feat: Getting Data

---
+++ src/app/posts/posts.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'posts',
+  templateUrl: './posts.component.html',
+  styleUrls: ['./posts.component.css']
+})
+export class PostsComponent {
+  posts;
+  /* For future visitors: In the new HttpClient (Angular 4.3+), the response object is JSON by default,
+   so you don't need to do response.json().data anymore. Just use response directly. */
+  constructor(http: HttpClient) {
+    http.get('http://jsonplaceholder.typicode.com/posts').subscribe(response => {
+      this.posts = response
+      console.log(this.posts);
+    });
+    /* subscribe(
+      response => console.log(response)
+    ); */
+  }
+
+}

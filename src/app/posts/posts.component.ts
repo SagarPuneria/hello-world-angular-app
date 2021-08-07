2f83a93fd798fc973ac2c41560f1478ae0907e25
Sagar Puneria <sagar.puneria@gmail.com>
2021-08-07 19:47:44 +0530

    feat: Updating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -36,4 +36,13 @@ export class PostsComponent {
         console.log(this.posts);
       });
   }
+
+  updatePost(post) {
+    this.http.patch(this.url + '/' + post['id'], JSON.stringify({ isRead: true }))
+      .subscribe(response => {
+        console.log(response);
+      });
+    // OR
+    // this.http.put(this.url, JSON.stringify(post));
+  }
 }

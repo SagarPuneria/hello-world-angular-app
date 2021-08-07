e22a529580b208f0cc34f67fc554e1d07bf5cdc0
Sagar Puneria <sagar.puneria@gmail.com>
2021-08-07 22:28:17 +0530

    feat: OnInit Interface

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,26 +1,41 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
-export class PostsComponent {
+export class PostsComponent implements OnInit {
   posts;
   private url = 'http://jsonplaceholder.typicode.com/posts';
-  /* For future visitors: In the new HttpClient (Angular 4.3+), the response object is JSON by default,
-   so you don't need to do response.json().data anymore. Just use response directly. */
+
+
+  // In Angular2+ we use constructor to do the DI(Dependency Injection).
+  // Called first time before the ngOnInit().
   constructor(private http: HttpClient) {
+    /*
+    // We shouldn't perform expensive operations like calling the server.
     http.get(this.url).subscribe(response => {
       console.log(typeof this.posts); // undefined
       this.posts = response;
       console.log(typeof this.posts); // object
       console.log(this.posts);
-    });
-    /* subscribe(
+    });*/
+    /* For future visitors: In the new HttpClient (Angular 4.3+), the response object is JSON by default,
+       so you don't need to do response.json().data anymore. Just use response directly. */
+    /* .subscribe(
       response => console.log(response)
     ); */
+  }
+  ngOnInit() {
+    // Called after the constructor and called  after the first ngOnChanges()
+    this.http.get(this.url).subscribe(response => {
+      console.log(typeof this.posts); // undefined
+      this.posts = response;
+      console.log(typeof this.posts); // object
+      console.log(this.posts);
+    });
   }
 
   createPost(input: HTMLInputElement) {

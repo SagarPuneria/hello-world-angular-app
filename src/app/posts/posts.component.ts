c2c725c66c05ae08aad944db67ebc098c7dad876
Sagar Puneria <sagar.puneria@gmail.com>
2021-08-07 15:51:46 +0530

    feat: Creating Data

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,12 +8,13 @@ import { Component } from '@angular/core';
 })
 export class PostsComponent {
   posts;
+  private url = 'http://jsonplaceholder.typicode.com/posts';
   /* For future visitors: In the new HttpClient (Angular 4.3+), the response object is JSON by default,
    so you don't need to do response.json().data anymore. Just use response directly. */
-  constructor(http: HttpClient) {
-    http.get('http://jsonplaceholder.typicode.com/posts').subscribe(response => {
+  constructor(private http: HttpClient) {
+    http.get(this.url).subscribe(response => {
       console.log(typeof this.posts); // undefined
-      this.posts = response
+      this.posts = response;
       console.log(typeof this.posts); // object
       console.log(this.posts);
     });
@@ -22,4 +23,17 @@ export class PostsComponent {
     ); */
   }
 
+  createPost(input: HTMLInputElement) {
+    // let post: any = { title: input.value } // => post.id = response['id'];
+
+    let post = { title: input.value };
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post))
+      .subscribe(response => {
+        post['id'] = response['id'];
+        this.posts.splice(0, 0, post);
+        // console.log(response['id']);
+        console.log(this.posts);
+      });
+  }
 }
